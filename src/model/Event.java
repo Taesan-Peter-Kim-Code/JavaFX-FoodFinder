79d341e374ed5635e1bc43987fa8873c3160aa6f
Taesan Kim <peterkim3002@gmail.com>
2020-12-03 11:48:39 -0500

    Added variables for event class

---
+++ src/model/Event.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author taesankim,
+ */
+public class Event {
+    
+    private String eventName;
+    private String organization;
+    private String date;
+    private String time;
+    private String location;
+    private String description;
+
+    public Event(String eventName, String organization, String date, String time, String location, String description) {
+        this.eventName = eventName;
+        this.organization = organization;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.description = description;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+}
