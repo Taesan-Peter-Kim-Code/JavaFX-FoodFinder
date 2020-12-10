ed358398c202c27f3dc87916f774b32ba2261982
Hayden Long <hql5426@psu.edu>
2020-12-10 17:20:34 -0500

    Changed the view to be more traditional and updated the controller

--- FoodFinder/src/controller/FreeFoodMainViewController.java
+++ FoodFinder/src/controller/FreeFoodMainViewController.java
@@ -1,147 +1,120 @@
 package controller;
 
-import java.awt.event.MouseEvent;
-import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import model.Event;
 import model.Usermodel;
 
-public class FreeFoodMainViewController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private TableColumn<?, ?> eventNameColumn;
-
-    @FXML
-    private Label dateLabel;
-
-    @FXML
-    private Button myEventsButton;
-
-    @FXML
-    private Label organizationName;
-
-    @FXML
-    private Button logOutButton;
-
-    @FXML
-    private Label eventName;
-
-    @FXML
-    private Label locationLabel;
-
-    @FXML
-    private TableView<?> eventTable;
-
-    @FXML
-    private GridPane detailsGrid;
-
-    @FXML
-    private Label timeLabel;
-
-    @FXML
-    private Label descriptionLabel;
-
-    @FXML
-    private Button homeButton;
-    
-    private Usermodel activeUser;
-
-    @FXML
-    void scrollDirection(ActionEvent event) {
-
-    }
-    public Usermodel getActiveUser()
-    {
-        return activeUser;
-    }
-
-    public void setActiveUser(Usermodel activeUser)
-    {
-        this.activeUser = activeUser;
-    }
-    @FXML
-    void showDetails(MouseEvent event, Event model) {
-        Event selectedModel = model;
-        eventName.setText(model.getId().toString());
-        organizationName.setText(model.getOrganizationname());
-        dateLabel.setText(model.getDate().toString());
-        timeLabel.setText(model.getTime().toString());
-        locationLabel.setText(model.getLocation());
-        descriptionLabel.setText(model.getDescription());
-    }
+/**
+ *
+ * @author hayde
+ */
+public class FreeFoodMainViewController implements Initializable {
     
     @FXML
-    void d2c7bd(ActionEvent event) {
+    private TableView<Event> eventTable;
 
+    @FXML
+    private TableColumn<Event, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Event, String> eventNameColumn;
+
+    @FXML
+    private TableColumn<Event, String> orgColumn;
+
+    @FXML
+    private TableColumn<Event, String> dateColumn;
+
+    @FXML
+    private TableColumn<Event, String> timeColumn;
+
+    @FXML
+    private TableColumn<Event, String> desColumn;
+
+    @FXML
+    private Button loadBtn;
+
+    @FXML
+    private Button saveBtn;
+    
+    private EntityManager myManager;
+    
+    private ObservableList<Event> eventData;
+    
+    private List<Event> events;
+    
+    public void setTableData(List<Event> eventList) {
+        
+        eventData = FXCollections.observableArrayList();
+        
+
+        eventList.forEach(s -> {
+            eventData.add(s);
+        });
+        
+        eventTable.setItems(eventData);
+        eventTable.refresh();
+       
+        
+    }
+    
+
+    @FXML
+    void loadData(ActionEvent event) {
+        
+        events = readAll();
+        
+        setTableData(events);
     }
 
     @FXML
     void saveEventBtn(ActionEvent event) {
-
+        System.out.println("Save button clicked");
     }
 
-    @FXML
-    void showView(ActionEvent event) throws IOException {
-        String view = (String) ((Node) event.getSource()).getUserData();
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(view));
-
-        Parent root = loader.load();
-
-        Scene saveScene = new Scene(root);
-
-        Scene currentScene = ((Node) event.getSource()).getScene();
-
-        Stage stage = (Stage) currentScene.getWindow();
-
-        stage.setScene(saveScene);
-        stage.show();
+        myManager = (EntityManager) Persistence.createEntityManagerFactory("FoodFinderPU").createEntityManager();
+        
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventname"));
+        orgColumn.setCellValueFactory(new PropertyValueFactory<>("organization"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+        desColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
         
         
-    }
-
-    @FXML
-    void initialize() {
-        assert eventNameColumn != null : "fx:id=\"eventNameColumn\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert dateLabel != null : "fx:id=\"dateLabel\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert myEventsButton != null : "fx:id=\"myEventsButton\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert organizationName != null : "fx:id=\"organizationName\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert logOutButton != null : "fx:id=\"logOutButton\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert eventName != null : "fx:id=\"eventName\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert locationLabel != null : "fx:id=\"locationLabel\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert eventTable != null : "fx:id=\"eventTable\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert detailsGrid != null : "fx:id=\"detailsGrid\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert timeLabel != null : "fx:id=\"timeLabel\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert descriptionLabel != null : "fx:id=\"descriptionLabel\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'FreeFoodMainView.fxml'.";
-        
-        EntityManager myManager = (EntityManager) Persistence.createEntityManagerFactory("FoodFinderPU").createEntityManager();
-        eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventName"));
-
         eventTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        
 
+}
+    
+    @FXML
+     public List readAll(){
+         
+        Query query = myManager.createNamedQuery("Event.findAll");
+               List<Event> events = query.getResultList();
+        return events;
     }
+
+    void setActiveUser(Usermodel user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
