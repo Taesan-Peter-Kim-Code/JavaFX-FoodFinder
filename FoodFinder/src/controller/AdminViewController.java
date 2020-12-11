88dad7d9bbf0296e51500dcb615b49f72c11d83b
Hayden Long <hql5426@psu.edu>
2020-12-11 17:57:15 -0500

    Gave AdminView functionality, added NewEventView but it needs work in order to work right

---
+++ FoodFinder/src/controller/AdminViewController.java
@@ -0,0 +1,217 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author hayde
+ */
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import model.Event;
+import controller.NewEventController;
+import java.io.IOException;
+import javafx.stage.Stage;
+
+
+public class AdminViewController implements Initializable {
+    
+     @FXML
+    private TableView<Event> eventView;
+
+    @FXML
+    private TableColumn<Event, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Event, String> eventNameColumn;
+
+    @FXML
+    private TableColumn<Event, String> orgColumn;
+
+    @FXML
+    private TableColumn<Event, String> dateColumn;
+
+    @FXML
+    private TableColumn<Event, String> timeColumn;
+
+    @FXML
+    private TableColumn<Event, String> desColumn;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button readButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button updateButton;
+    
+    private EntityManager myManager;
+    
+    private ObservableList <Event> eventData;
+    
+    private List<Event> events;
+    
+    
+    public void setTableData(List<Event> eventList) {
+        
+        eventData = FXCollections.observableArrayList();
+        
+
+        eventList.forEach(s -> {
+            eventData.add(s);
+        });
+        
+        eventView.setItems(eventData);
+        eventView.refresh();
+       
+        
+    }
+
+    @FXML
+    void createEvent(ActionEvent event) throws IOException {
+        System.out.println("Creating new event");
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/NewEventView.fxml"));
+
+        Parent newEventView = loader.load();
+
+        Scene tableViewScene = new Scene(newEventView);
+
+        NewEventController newEventController = loader.getController();
+        
+        Stage stage = new Stage();
+        stage.setScene(tableViewScene);
+        stage.show();
+        
+        int id = 0; 
+        
+        Event newEvent = new Event();
+        
+        for(Event e: eventData){
+            if(id == e.getId()){
+                id++;
+            }
+        }
+        newEvent.setId(id);
+        
+        newEventController.createEvent(event, id, newEvent);
+        /*
+        newEvent.setEventname(eventName);
+        newEvent.setOrganization(organization);
+        newEvent.setDate(date);
+        newEvent.setTime(time);
+        newEvent.setDescription(description);
+               
+        create(newEvent);
+*/
+        eventView.refresh();
+
+    }
+
+    @FXML
+    void deleteEvent(ActionEvent event) {
+        
+       Event e = eventView.getSelectionModel().getSelectedItem();
+       
+       System.out.println("we are deleting this student: "+ e.toString());
+       delete(e);
+
+    }
+
+    @FXML
+    void readEvents(ActionEvent event) {
+        events = readAll();
+        
+        setTableData(events);
+        
+    }
+
+    @FXML
+    void updateEvent(ActionEvent event) {
+
+    }
+    
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        myManager = (EntityManager) Persistence.createEntityManagerFactory("FoodFinderPU").createEntityManager();
+        
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventname"));
+        orgColumn.setCellValueFactory(new PropertyValueFactory<>("organization"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
+        desColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        
+        
+        eventView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        
+
+}
+    public void create(Event newEvent) {
+        try {
+            myManager.getTransaction().begin();
+            
+            if (newEvent.getId() != null) {
+                
+                myManager.persist(newEvent);
+                
+                myManager.getTransaction().commit();
+                
+                System.out.println(newEvent.toString() + " is created");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public List<Event> readAll(){
+        Query query = myManager.createNamedQuery("Event.findAll");
+        List<Event> events = query.getResultList();
+        
+        return events;
+    }
+    
+    public void delete(Event selectedEvent) {
+        try {
+           
+                myManager.getTransaction().begin();
+                
+    
+                myManager.remove(selectedEvent);
+                
+      
+                myManager.getTransaction().commit();
+ 
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
+
---
+++ FoodFinder/src/controller/NewEventController.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author hayde
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.Event;
+import controller.FreeFoodMainViewController;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class NewEventController {
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField eventNameField;
+
+    @FXML
+    private TextField organizationField;
+
+    @FXML
+    private TextField dateField;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private TextArea descriptionField;
+
+    @FXML
+    private Button saveButton;
+    
+    private AdminViewController mainAdminController;
+    
+    private int id;
+    
+    private Event newEvent;
+    
+
+    @FXML
+    void createEvent(ActionEvent event, int id, Event newEvent) throws IOException {
+        
+        id = this.id;
+        newEvent = this.newEvent;
+        String eventName = eventNameField.getText();
+        String organization = organizationField.getText();
+        String date = dateField.getText();
+        String time = timeField.getText();
+        String description = descriptionField.getText();
+        
+        newEvent.setEventname(eventName);
+        newEvent.setOrganization(organization);
+        newEvent.setDate(date);
+        newEvent.setTime(time);
+        newEvent.setDescription(description);
+               
+        mainAdminController.create(newEvent);
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/AdminView.fxml"));
+
+        Parent adminView = loader.load();
+
+        Scene tableViewScene = new Scene(adminView);
+
+        AdminViewController adminController = loader.getController();
+    }
+
+    public TextField getIdField() {
+        return idField;
+    }
+
+    public void setIdField(TextField idField) {
+        this.idField = idField;
+    }
+
+    public TextField getEventNameField() {
+        return eventNameField;
+    }
+
+    public void setEventNameField(TextField eventNameField) {
+        this.eventNameField = eventNameField;
+    }
+
+    public TextField getOrganizationField() {
+        return organizationField;
+    }
+
+    public void setOrganizationField(TextField organizationField) {
+        this.organizationField = organizationField;
+    }
+
+    public TextField getDateField() {
+        return dateField;
+    }
+
+    public void setDateField(TextField dateField) {
+        this.dateField = dateField;
+    }
+
+    public TextField getTimeField() {
+        return timeField;
+    }
+
+    public void setTimeField(TextField timeField) {
+        this.timeField = timeField;
+    }
+
+    public TextArea getDescriptionField() {
+        return descriptionField;
+    }
+
+    public void setDescriptionField(TextArea descriptionField) {
+        this.descriptionField = descriptionField;
+    }
+
+    public Button getSaveButton() {
+        return saveButton;
+    }
+
+    public void setSaveButton(Button saveButton) {
+        this.saveButton = saveButton;
+    }
+    
+
+}
